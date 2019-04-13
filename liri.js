04de33de02f68cd49e69278f2d8719828384b7d1
Prasen Gope <prasen.gope@hotmail.com>
2019-04-13 18:47:49 -0400

    got movies.js to work, yay!!

--- liri.js
+++ liri.js
@@ -1,8 +1,11 @@
+//requiring dotenv
+require('dotenv').config();
+
 //requiring file system
-let fs = require('fs');
+var fs = require('fs');
 
 //requiring the movies.js file
-let myMovies = require('./movies.js');
+var myMovies = require('./movies.js');
 
 //storing all words from the user
 var userCommand = process.argv[2];
@@ -11,3 +14,23 @@ var userCommand = process.argv[2];
 //creating an usable user input from the user commands
 var userInput = process.argv.splice(3, process.argv.length).join(' ');
 // console.log(userInput);
+
+//Program conditions
+switch (userCommand) {
+	// help function to clarify commands used
+	case 'help':
+		console.log(
+			'Please type one of these commands\n' +
+				"'concert-this': to search your favorite artist concerts\n" +
+				"'spotify-this-song': to search your favorite song\n" +
+				"'movie-this': to search your favorite movie \n" +
+				"'do-what-it-says': using command from random.txt \n"
+		);
+		break;
+	case 'movie-this':
+		myMovies(userInput);
+		break;
+	//if anything else written
+	default:
+		console.log("LIRI doesn't understand that - Please type 'node liri.js help' for more information");
+};
--- movies.js
+++ movies.js
@@ -1,9 +1,48 @@
+//requiring key.js
+var keys = require("./keys.js");
+
+//requiring file system
+var fs = require("fs");
+
+//requiring axios
 var axios = require("axios");
 
+console.log("movies.js is connected!")
 
-function myMovies
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    console.log("The movie's rating is: " + response.data.imdbRating);
-  }
-);
+
+
+function myMovies(userInput) {
+    var movie = userInput;
+    if (!movie) {
+        console.log("If you have not watched 'Mr. Nobody', you should.\n");
+        console.log("It's on Netfllix.");
+        movie = 'Mr. Nobody';
+    }
+
+    var url = 'https://www.omdbapi.com/?t=' + movie + '&y=&plot=short&apikey=' + keys.omdb.id;
+
+    axios.get(url).then(function (response) {
+        console.log("The movie's rating is: " + response.data.imdbRating);
+        console.log('The movie you searched for: ' + movie);
+    },
+
+        function (error) {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        }
+    );
+};
+
+//Exporting the function which we will use in liri.js
+module.exports = myMovies;
